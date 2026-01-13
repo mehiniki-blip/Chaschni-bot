ad827781904413284a7aca096abcc1daea7d8b8e
mehiniki-blip <mehiniki@gmail.com>
2026-01-13 21:33:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from flask import Flask
 import os
 import time
 import sqlite3
@@ -653,6 +654,21 @@ def main():
 
     print("Bot is running...")
 
+    # --------- FAKE WEB SERVER FOR RENDER ----------
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+def run_server():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
+
+threading.Thread(target=run_server).start()
+# ------------------------------------------------
     updater.start_polling()
     updater.idle()
 
